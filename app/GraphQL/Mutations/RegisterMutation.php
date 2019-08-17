5a0b2b0cbee8f6780a78bbcf683d413135c91766
dali546 <35352237+dali546@users.noreply.github.com>
2019-08-17 16:50:25 +0100

    Added Register Mutation
    Added Register Mutation and login automatically after registering
    TODO - Add more robust error handling etc... currently client will handle all errors

--- app/GraphQL/Mutations/RegisterMutation.php
+++ app/GraphQL/Mutations/RegisterMutation.php
@@ -3,9 +3,10 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\User;
+use Exception;
 use GraphQL\Type\Definition\ResolveInfo;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
+use Joselfonseca\LighthouseGraphQLPassport\GraphQL\Mutations\Login;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class RegisterMutation {
@@ -14,22 +15,23 @@ class RegisterMutation {
      *
      * @param null $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
      * @param mixed[] $args The arguments that were passed into the field.
-     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext $context Arbitrary data that is shared between all fields of a single query.
-     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @param GraphQLContext $context Arbitrary data that is shared between all fields of a single query.
+     * @param ResolveInfo $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
      * @return mixed
+     * @throws Exception
      */
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
-        event(new Registered(
-            User::create([
-                'username' => $args['data']['username'],
-                'email' => $args['data']['email'],
-                'password' => Hash::make($args['data']['password'])
-            ])
-        ));
 
-        return ['status' => 'success',
-            'message' => 'Registration was successful! Please use the below link to go to the login page',
-            'redirect' => 'http://localhost:8000/'
-        ];
+        // TODO Add proper validating/resolving
+        User::create([
+            'username' => $args['data']['username'],
+            'email' => $args['data']['email'],
+            'password' => Hash::make($args['data']['password'])
+        ]);
+
+
+        // Login...
+        $login = new Login();
+        return $login->resolve($rootValue, $args, $context, $resolveInfo);
     }
 }
